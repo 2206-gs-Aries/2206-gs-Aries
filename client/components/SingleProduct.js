caa3c703dc115f1ad31d24c1c3c0d78640583f6a
Kyle Parkinson <kparki1130@gmail.com>
2022-08-04 17:12:16 -0400

    product component done. needs css

--- client/components/SingleProduct.js
+++ client/components/SingleProduct.js
@@ -1,31 +1,24 @@
 import React from "react";
-import {Link} from 'react-router-dom'
+import { Link } from "react-router-dom";
 
+export class SingleProduct extends React.Component {
+  render() {
+    return (
+      <div className="product">
+        <div className="product_info">
+          <Link to={`/home/${this.props.id}`}>{this.props.name}</Link>
 
-export class SignleProduct extends React.Component {
-    render() {
-        return (
-            <div className="product">
-                <div className="product_info">
-                <Link to={`/home/${this.props.id}`}>
-                        { this.props.name }
-                        </Link>
+          <p className="product_price">
+            <small>$</small>
+            <strong>{this.props.price}</strong>
+          </p>
+        </div>
+        <img src={this.props.imageUrl} />
 
-                    <p className="product_price">
-                        <small>$</small>
-                        <strong>{ this.props.price }</strong>
-                    </p>
-
-                </div>
-                <img src={this.props.imageUrl} />
-
-                <button className="addCart">Add to Cart</button>
-          
-            </div>
-        )
-    }
+        <button className="addCart">Add to Cart</button>
+      </div>
+    );
+  }
 }
 
-
-
-export default SignleProduct
+export default SingleProduct;
